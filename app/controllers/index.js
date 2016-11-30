8482b724c95f534de41461b87c5d69813ae6d234
Arun Shankar <arun@fuiszmedia.com>
2016-11-29 16:15:04 -0800

    create backend endpoints for creating a new user and login

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,46 +3,17 @@ var router = express.Router();
 var passport = require('passport')
 
 var Account = require('../models/User')
-/* GET home page. */
+
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
 
 var users = require('./Users')
+router.get('/', (req, res) => res.render('index', {}))
+router.get('/register', (req, res) =>  res.render('create', {}))
+router.post('/register', (req, res, next) => users.createUser(req, res, next))
 
-router.get('/', (req, res) => {
-  res.render('index', { user : req.user });
-})
-
-router.get('/register', (req, res) => {
-  res.render('create', {});
-})
-
-router.post('/register', (req, res, next) => {
-  const username = req.body.username
-  const password = req.body.password
-  const user = {
-    username,
-    password
-  }
-  var newUser = new Account(user)
-  const promise = newUser.save()
-  promise.then((user) => {
-    return res.redirect('/login')
-  })
-})
-
-router.get('/login', (req, res) => {
-  res.render('index', { user : req.user });
-})
-
-router.post('/login', (req, res, next) => {
-  req.session.save((err) => {
-    if (err) {
-      return next(err)
-    }
-    res.redirect('/')
-  })
-})
+router.get('/login', (req, res) => res.render('index', {}))
+router.post('/login', (req, res, next) => users.login(req, res, next))
 
 module.exports = router;
